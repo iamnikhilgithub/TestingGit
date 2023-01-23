aa1b8bab8890b18aff752a02217221b2922b3098
nikhil <iamnikhil01@gmail.com>
2023-01-23 14:12:47 +0530

    changed

--- src/com/velocity/Test.java
+++ src/com/velocity/Test.java
@@ -5,7 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
    System.out.println("git testing");
-   System.out.println("my name is nikhil");
+   System.out.println("my name is nikhil12");
 	}
 
 }
